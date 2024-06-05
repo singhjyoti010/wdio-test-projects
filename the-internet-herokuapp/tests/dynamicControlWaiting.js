d7c811003ff87d58c4c6f9d3b546e0eed13e5723
Jyoti Singh <singhjyoti010@outlook.com>
2024-06-05 14:03:54 +0530

    createad dynamicControlwaiting.js for dynamic content and updated scrollToLandingPage to use waitForDisplayed options

---
+++ the-internet-herokuapp/tests/dynamicControlWaiting.js
@@ -0,0 +1,7 @@
+describe("waiting stretegies for dynamic control", () => {
+    it("should wait for element to be enabled then click", async () => {
+        //below is used to open browser in debug mode
+        browser.debug();
+        //inprogress
+    })
+})
--- the-internet-herokuapp/tests/scrollToLandingPageFooter.js
+++ the-internet-herokuapp/tests/scrollToLandingPageFooter.js
@@ -3,6 +3,7 @@ import { Footer, LandingPage } from "../helpers/hooks/Internet.js"
 describe("Scroll to API Action", () => {
     it("should scroll to page footer", async () => {
         await browser.url("/");
+        await (await LandingPage.headingText).waitForDisplayed({timeout:1000, timeoutMsg: "element not displaeyd"});
         await Footer.landingPageFooter.moveTo(1,1);
         expect(Footer.landingPageFooter).toBeDisplayedInViewport();
     })
